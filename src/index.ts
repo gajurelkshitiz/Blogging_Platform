5558f627e9d22f08b44f7ec79b9283013de083d2
gajurelkshitiz <kshitiz.purbct042@gmail.com>
2025-09-12 08:31:57 +0545

    connected postgres with local db, and created user: blog_user for blog db

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
 import express, {Request, Response} from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+app.use(helmet());
+
 
 
 app.get('/test', (req: Request, res: Response) => {
   res.send('Hello test route.');
 });
 
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`app running on http://localhost:${PORT}`);
 });
